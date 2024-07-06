9a69c179a4d75d415ebf7ca11e006c15c17f3c5c
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-07-06 16:45:03 +0900

    🔧 Test

--- week2_back/test_app/models.py
+++ week2_back/test_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Author(models.Model):
     first_name = models.CharField(max_length=28)
-    last_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=28)
     birthdate = models.DateField()
 
     def __str__(self):
