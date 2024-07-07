841589df5d0e70692980fad0ce0d59aa4f307ce4
eun-young <eunyoung12@kaist.ac.kr>
2024-07-08 01:20:29 +0900

    <feat> setting, url 수정

--- week2_back/week2_back/urls.py
+++ week2_back/week2_back/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("test_app/", include("test_app.urls")),
     path("admin/", admin.site.urls),
+    path("MyAvatar/", include("MyAvatar.urls"))
 ]
