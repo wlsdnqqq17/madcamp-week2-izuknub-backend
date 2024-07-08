9ad1c02d04e42045d692eb9239d50a340a8e2bd1
eun-young <eunyoung12@kaist.ac.kr>
2024-07-09 04:23:26 +0900

    <fix> urls.py 수정

--- week2_back/MyAvatar/urls.py
+++ week2_back/MyAvatar/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('add_friend/', views.add_friend, name='add_friend'),
     path('get_friends/<str:user_id>', views.get_friends, name='get_friends'),
     path('get_friend_requests/<str:user_id>', views.get_friend_requests, name='get_friend_requests'),
-    path('accept_friend_request/', accept_friend_request, name='accept_friend_request'),
+    path('accept_friend_request/', views.accept_friend_request, name='accept_friend_request'),
 ]
