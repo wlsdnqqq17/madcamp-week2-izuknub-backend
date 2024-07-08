05c12110b2958e5483bb86fb8a5a13fcfe61251a
eun-young <eunyoung12@kaist.ac.kr>
2024-07-09 02:41:10 +0900

    <feat> 친구신청 기능 구현 친구 신청이 들어오면 Friend table이 업데이트되도록 구현

--- week2_back/MyAvatar/urls.py
+++ week2_back/MyAvatar/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('save_kakao_user/', views.save_kakao_user, name='save_kakao_user'),
     path('search_user/', views.search_user, name='search_user'),
+    path('get_items/', views.get_items, name='get_items'),
+    path('add_friend/', views.add_friend, name='add_friend'),
 ]
